b7377d7dd8e66839e7d1855d26711768906b68dd
santi <santiagostino@users.noreply.github.com>
2024-12-05 22:26:52 -0800

    Updated az

--- vpc/main.tf
+++ vpc/main.tf
@@ -15,7 +15,7 @@ resource "aws_subnet" "pb_sn" {
     vpc_id = aws_vpc.nedsvpc.id
     cidr_block = "10.0.1.0/24"
     map_public_ip_on_launch = true
-    availability_zone = "us-west-2-a"
+    availability_zone = "us-west-2a"
     
     tags = {
 
